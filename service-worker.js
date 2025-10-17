f6d3c101db089dcb96ba750b1508d8fde26fd350
nss102082m-a11y <nss102082m@gmail.com>
2025-10-17 21:29:43 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js  — 完成版
-const VERSION = 'v5';                         // ★ここだけ上げる
+const VERSION = 'v6';                         // ★ここだけ上げる
 const CACHE_NAME = `islai-${VERSION}`;
 const ORIGIN = self.location.origin;
 
